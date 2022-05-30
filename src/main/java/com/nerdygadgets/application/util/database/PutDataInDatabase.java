52511cda0015cf7b4757e534c01354ca2c9f6579
404projects <90609854+404projects@users.noreply.github.com>
2022-05-30 19:59:04 +0200

    upload database utilss

--- src/main/java/com/nerdygadgets/application/util/database/PutDataInDatabase.java
+++ src/main/java/com/nerdygadgets/application/util/database/PutDataInDatabase.java
@@ -11,31 +11,31 @@ public class PutDataInDatabase
 
     public static <T extends NetworkComponent> void updateData(T component)
     {
-        try
-        {
-            Connection connection = ConnectionToDatabase.DatabaseConnection();
-
-            String type = component.getType().name().toLowerCase();
-            if (type.equals("database"))
-            {
-                type = "database1";
-            }
-
-            String sql = String.format("INSERT INTO %s (name, availability, price, ip, subnetmask)", type) + "values (?,?,?,?,?)";
-
-            PreparedStatement preparedStmt = connection.prepareStatement(sql);
-            preparedStmt.setString(1, component.getName());
-            preparedStmt.setDouble(2, component.getAvailability());
-            preparedStmt.setDouble(3, component.getPrice());
-            preparedStmt.setString(4, component.getIp());
-            preparedStmt.setString(5, component.getSubnetMask());
-
-            preparedStmt.execute();
-            connection.close();
-        } catch (Exception e)
-        {
-            e.printStackTrace();
-        }
+//        try
+//        {
+//            Connection connection = ConnectionToDatabase.DatabaseConnection();
+//
+//            String type = component.getType().name().toLowerCase();
+//            if (type.equals("database"))
+//            {
+//                type = "database1";
+//            }
+//
+//            String sql = String.format("INSERT INTO %s (name, availability, price, ip, subnetmask)", type) + "values (?,?,?,?,?)";
+//
+//            PreparedStatement preparedStmt = connection.prepareStatement(sql);
+//            preparedStmt.setString(1, component.getName());
+//            preparedStmt.setDouble(2, component.getAvailability());
+//            preparedStmt.setDouble(3, component.getPrice());
+//            preparedStmt.setString(4, component.getIp());
+//            preparedStmt.setString(5, component.getSubnetMask());
+//
+//            preparedStmt.execute();
+//            connection.close();
+//        } catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
     }
 
 }
